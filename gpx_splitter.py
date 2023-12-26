2e295e776f432a2eed6f27f945889ff39ea101e4
Brett Wines <bwines@quip.com>
2023-12-26 11:41:21 -0500

    fancier prompts

--- gpx_splitter.py
+++ gpx_splitter.py
@@ -5,24 +5,79 @@ import datetime
 import re
 import os
 import sys
+import time
 import xml.etree.ElementTree as ET
+
+
+# GPX structure from GaiaGPS files:
+#
+# Root node:
+# [('creator', 'GaiaGPS'),
+#  ('version', '1.1'),
+#  ('{http://www.w3.org/2001/XMLSchema-instance}schemaLocation',
+#   'http://www.topografix.com/GPX/1/1
+#    http://www.topografix.com/GPX/1/1/gpx.xsd')]
+#
+# Coordinate-timestamp pairs live at tree.getroot()[0][3]
+# >>> p = tree.getroot()[0][3][0]
+# >>> x = p[0]
+# >>> y = p[1]
+# >>> x.text
+# '1213.3' # not sure yet what this is
+# >>> y.text
+# '2023-10-29T12:27:17Z'
+# >>>
+# >>> p.items()
+# [('lat', '37.734814'), ('lon', '-119.566125')]
 
 
 TIMESTAMP_EX = "TODO"
 LATLONGS_CHILDREN_TAG = "{http://www.topografix.com/GPX/1/1}trkpt"
+BACKUP_NAMESPACE = "http://www.topografix.com/GPX/1/1"  # Gaia GPS
+UTC_SUFFIX = "+00:00"
+GPX_FILE_RE = re.compile(r"(.*)\.gpx")
+TEXT_BOLD = "\033[1m"
+TEXT_NORMAL = "\033[0m"
 
-def help():
-    print("Usage: ./gpx_splitter.py my_file.gpx timestamp1 timestamp2 ..."
-          "\n       ./gpx_splitter.py help")
-    print(f"\nTimestamps should be in NOCOMMIT format, e.g. {TIMESTAMP_EX}")
 
+def sys_tz():
+    # UTC fortunately does not observe daylight savings time
+    return time.tzname[0]
+
+
+def bold(s):
+    return f"{TEXT_BOLD}{s}{TEXT_NORMAL}"
+
+
+def utc_to_local(utc_dt):
+    return utc_dt.replace(tzinfo=datetime.timezone.utc).astimezone(tz=None)
+
+
+
+def print_file_summary(tree):
+    start, end = parse_for_boundary_timestamps(tree)
+    span = end - start
+    creator = tree.getroot().get("creator")
+    print(f"{sys.argv[1]} is a GPX file by {bold(creator)}."
+          f" It spans {bold(span)} hours.\n")
+
+    print(f"The start of the GPX file is {bold(utc_to_local(start))} ({sys_tz()})")
+    print(f"The   end of the GPX file is {bold(utc_to_local(end))} ({sys_tz()})")
 
 
 def gpx_time_string_to_timestamp(s):
     if s is None:
         return None
-    assert s.endswith("Z")
-    return datetime.datetime.fromisoformat(f"{s[:-1]}+00:00")
+    if s.endswith("Z"):
+        # fromisoformat() doesn't like the Z suffix, but it means UTC, so swap
+        # it out for +00:00
+        s = f"{s[:-1]}{UTC_SUFFIX}"
+    elif s.endswith(UTC_SUFFIX):
+        pass
+    else:
+        print("Unexpected non-UTC timezone. Treating as UTC; all times will be"
+              "off by a constant amount")
+    return datetime.datetime.fromisoformat(s)
 
 
 def rename_tree(tree, i):
@@ -32,7 +87,7 @@ def rename_tree(tree, i):
 
 def write_trees(trees):
     filename_base_fn = lambda i: re.match(
-        r"(.*)\.gpx", sys.argv[1]).groups()[0]
+        GPX_FILE_RE, sys.argv[1]).groups()[0]
     new_filename_fn = lambda i: f"{filename_base_fn(i)}-split-{i + 1}.gpx"
     for i in range(len(trees)):
         name = new_filename_fn(i)
@@ -47,21 +102,14 @@ def write_trees(trees):
         print(f"Wrote {name}")
 
 
-def split_gpx(start_s, end_s):
-    # latlongs live at tree.getroot()[0][3]
-    # >>> p = tree.getroot()[0][3][0]
-    # >>> x = p[0]
-    # >>> y = p[1]
-    # >>> x.text
-    # '1213.3'
-    # >>> y.text
-    # '2023-10-29T12:27:17Z'
-    # >>>
-    # >>> p.items()
-    # [('lat', '37.734814'), ('lon', '-119.566125')]
+def get_latlongs_node(tree):
+    return tree.getroot()[0][3]
 
+
+
+def split_gpx(start_s, end_s):
     tree = ET.parse(sys.argv[1])
-    latlongs_node = tree.getroot()[0][3]
+    latlongs_node = get_latlongs_node(tree)
     start = gpx_time_string_to_timestamp(start_s)
     end = gpx_time_string_to_timestamp(end_s)
     for e in latlongs_node.findall(LATLONGS_CHILDREN_TAG):
@@ -73,23 +121,52 @@ def split_gpx(start_s, end_s):
             latlongs_node.remove(e)
     return tree
 
+
+def register_namespace(tree):
+    # The purpose of this is so that output isn't full of "ns0"s
+    namespace_match = re.match(r"{(.*)}gpx", tree.getroot().tag)
+    if namespace_match is None:
+        print("Couldn't determine namespace from GPX file. Using"
+              " a backup namespace for our new files; this may not work.")
+        namespace = BACKUP_NAMESPACE
+    else:
+        namespace = namespace_match.groups()[0]
+    ET.register_namespace("", namespace)
+
+
+def parse_for_boundary_timestamps(tree):
+    latlongs_node = get_latlongs_node(tree)
+    nodes = latlongs_node.findall(LATLONGS_CHILDREN_TAG)
+    return (gpx_time_string_to_timestamp(nodes[0][1].text),
+            gpx_time_string_to_timestamp(nodes[-1][1].text))
+
+
+def prompt_user_for_timestamps(tree):
+    print("Please enter timestamps upon which to split, one by one,"
+          " in ISO 8601 format, e.g. 2023-10-29T14:25:33. Use your current"
+          f" local timezone ({sys_tz()}). Enter an empty response to indicate"
+          " when you're done.")
+    timestamps = []
+    while not timestamps or timestamps[-1]:
+        timestamps.append(input("... "))
+    return timestamps[:-1]
+
+
 def main():
-    if (len(sys.argv) == 2 and sys.argv[1] == "help"
-        or len(sys.argv) < 3
-        or not sys.argv[1].endswith(".gpx")):
-        help()
+    if len(sys.argv) != 2 or not sys.argv[1].endswith(".gpx"):
         sys.exit(1)
 
-    ET.register_namespace('', "http://www.topografix.com/GPX/1/1")
-    #ET.register_namespace('', "http://www.topografix.com/GPX/1/0")
+
     # TODO:
-    #   * inspect XML to find this^
     #   * interactively prompt user with time boundaries in their TZ
     #   * display???
-
+    tree = ET.parse(sys.argv[1])
+    print_file_summary(tree)
+    register_namespace(tree)
+    timestamps = [None] + prompt_user_for_timestamps(tree) + [None]
+    sys.exit(0)
 
     filename = sys.argv[1]
-    timestamps = [None] + sorted(sys.argv[2:]) + [None]
     trees = [split_gpx(ts[0], ts[1]) for ts in zip(timestamps, timestamps[1:])]
     write_trees(trees)
 
